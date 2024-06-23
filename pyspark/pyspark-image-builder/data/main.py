11aafad6df125b0a1008f191f9ef30ec4b2e1465
Ashborn013 <csarjun11@gmail.com>
2024-06-23 13:11:53 +0530

    added a regex quary , all_notable_event_id filter and deatact brute force funtions

--- pyspark/pyspark-image-builder/data/main.py
+++ pyspark/pyspark-image-builder/data/main.py
@@ -27,6 +27,7 @@ df_selected = df.select(
     col("host").getItem("hostname").alias("hostname"),
 )
 
+# ----------------- funtions  -----------------
 
 def filter_logs_by_event_id(df, event_id):
     return df.filter(col("event_id") == event_id)
@@ -44,8 +45,129 @@ def rule_engine(df, rules):
             df = count_logs_by_hostname(df)
     return df
 
+def regex_query(df, query_list):
+    result_df = None
 
-# rules
+    for query in query_list:
+        filtered_df = df.filter(col("message").rlike(query))
+
+        if result_df is None:
+            result_df = filtered_df
+        else:
+            result_df = result_df.union(filtered_df)
+
+    if result_df is not None:
+        for field in result_df.schema.fields:
+            if field.dataType.simpleString().startswith("map"):
+                result_df = result_df.withColumn(field.name, to_json(col(field.name)))
+        result_df = result_df.dropDuplicates()
+        # result_df.select("message").show(truncate=False)
+        result_df.show(truncate=True)
+        return result_df
+
+    else:
+        print("No matches found.")
+        return None
+
+
+def all_notable_event_id(df):
+    ids = [
+        27,
+        104,
+        140,
+        1001,
+        4624,
+        4625,
+        4648,
+        4649,
+        4657,
+        4670,
+        4703,
+        4713,
+        4717,
+        4718,
+        4725,
+        4732,
+        4739,
+        4769,
+        4771,
+        4776,
+        4781,
+        4782,
+        4782,
+        4798,
+        4816,
+        4946,
+        4947,
+        4948,
+        5025,
+        5027,
+        5034,
+        5142,
+        6145,
+        6273,
+        6416,
+        6423,
+        7023,
+        7045,
+        24577,
+        32850,
+    ]
+    union_df = None 
+    for i in ids:
+        df_filter = filter_logs_by_event_id(df, i)
+        if union_df is None:
+            union_df = df_filter
+        else:
+            union_df = union_df.union(df_filter)
+
+    print(union_df.count())
+    return union_df
+
+
+def detact_bruteForce(df):
+    df = df.withColumn("@timestamp", col("@timestamp").cast(TimestampType()))
+    out_put = filter_logs_by_event_id(df, 4625)
+    out_put = out_put.orderBy("@timestamp")
+
+    windowSpec = Window.orderBy("@timestamp")
+
+    out_put = out_put.withColumn(
+        "time_diff",
+        col("@timestamp").cast("long")
+        - lag("@timestamp", 1).over(windowSpec).cast("long"),
+    )
+
+    logs_under_one_min = out_put.filter(col("time_diff") < 60)
+
+    count = logs_under_one_min.count()
+    if count > 10:
+        print("Brute Force attampt deatacted .. !")
+        logs_under_one_min.show()
+        return logs_under_one_min
+    else:
+        return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ----------------- Main -----------------
+
 rules = [
     {"type": "filter_by_event_id", "event_id": "4625"},
     {"type": "count_by_hostname"},
