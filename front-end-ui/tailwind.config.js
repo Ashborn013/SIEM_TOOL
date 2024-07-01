361224fb3277aeadb89627d80e9be1d91c275198
Ashborn013 <csarjun11@gmail.com>
2024-07-01 16:06:41 +0530

    added daisy ui and react icons

--- front-end-ui/tailwind.config.js
+++ front-end-ui/tailwind.config.js
@@ -4,12 +4,38 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  // daisyui: {
+  //   themes: [
+  //     {
+  //       mytheme: {
+
+  //         "primary": "#0000ff",
+
+  //         "secondary": "#ff4a00",
+
+  //         "accent": "#94ad00",
+
+  //         "neutral": "#07090d",
+
+  //         "base-100": "#112636",
+
+  //         "info": "#00d0ff",
+
+  //         "success": "#24ce6d",
+
+  //         "warning": "#a08100",
+
+  //         "error": "#ff1151",
+  //       },
+  //     },
+  //   ],
+  // },
+  plugins: [require('daisyui')],
 }
