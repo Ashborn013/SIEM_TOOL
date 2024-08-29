7d3fbf81dc752b1e6cfc6d7551cb62d423414ce1
Ashborn013 <csarjun11@gmail.com>
2024-08-29 22:12:15 +0530

    added a search bar to reports page

--- front-end-ui-v2/src/app/user/report/page.js
+++ front-end-ui-v2/src/app/user/report/page.js
@@ -5,51 +5,75 @@ import NavBar from '@/components/NavBar';
 
 import Chart from 'chart.js/auto';
 
-
-
-
-
 export default function Page() {
-
   const [rows, setRows] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
   useEffect(() => {
-    function fetchJobDetails() {
-      fetch('http://127.0.0.1:223/Job_details')
-        .then(response => response.json())
-        .then(data => {
-          console.log("Fetched data");
-          setRows((currentRows) => {
-            if (JSON.stringify(currentRows) !== JSON.stringify(data)) {
-              // if (data.length > 0) {
-              CreateGraph(CountValuesInArray(data), "canva-bar", "doughnut");
-              // }
-              return data;
-            }
-            return currentRows;
-          });
-        })
-        .catch(error => console.error("Error fetching data:", error));
-    };
+    async function fetchJobDetails() {
+      try {
+        const response = await fetch('http://127.0.0.1:223/Job_details');
+        const data = await response.json();
+        console.log("Fetched data");
+
+        setRows((currentRows) => {
+          if (JSON.stringify(currentRows) !== JSON.stringify(data)) {
+            return data;
+          }
+          return currentRows;
+        });
+
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
 
     fetchJobDetails(); // Initial fetch
     const intervalId = setInterval(fetchJobDetails, 5000); // Fetch every 5000 ms (5 seconds)
     return () => clearInterval(intervalId);
   }, []);
 
+  const filteredRows = rows.filter(row => 
+    row.Job.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    row.message.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    row.level.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    row.Job_id.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    new Date(row.time * 1000).toLocaleString().toLowerCase().includes(searchQuery.toLowerCase())
+  );
+  
+  console.log(filteredRows)
 
   return (
     <div>
       <NavBar />
-      <div className='flex h-screen justify-evenly'>
-        <div className='flex-grow-1'>
-          <CreateTable rows={rows} />
-
+      <div className='flex h-screen justify-evenly flex-col p-2'>
+        <div className='flex justify-center mb-4'>
+          <label className="input input-bordered flex items-center gap-2 w-full max-w-md">
+            <input 
+              type="text" 
+              className="grow" 
+              placeholder="Search" 
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="h-4 w-4 opacity-70">
+              <path
+                fillRule="evenodd"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                clipRule="evenodd" />
+            </svg>
+          </label>
         </div>
-        <div className='flex-grow-2'>
+        <div className='flex-grow'>
+          <CreateTable rows={filteredRows} />
+        </div>
+        {/* <div className='flex-grow-2'>
           <canvas id="canva-bar"></canvas>
-
-        </div>
-
+        </div> */}
       </div>
       <SideBar />
     </div>
@@ -57,39 +81,11 @@ export default function Page() {
 }
 
 
-function CountValuesInArray(rows) {
-  let dict = {}
-  rows.forEach(row => {
-    let what = row.level;
-    dict[what] = (dict[what] || 0) + 1;
-  });
 
-  return dict
-}
-
-
-function CreateGraph(lablesPluseData, elmid, graph) {
-
-  const ctx = document.getElementById(elmid)
-  if (window.myChartInstance) {
-    window.myChartInstance.destroy();
-  }
-
-  window.myChartInstance = new Chart(ctx, {
-    type: graph,
-    data: {
-      labels: Object.keys(lablesPluseData),
-      datasets: [{
-        // label: 'lol my brain',
-        data: Object.values(lablesPluseData)
-      }]
-    }
-  })
-}
 
 
 function CreateTable({ rows }) {
-  return (<>
+  return (
     <div className="overflow-x-auto">
       <table className="table text-lg">
         {/* head */}
@@ -101,31 +97,21 @@ function CreateTable({ rows }) {
             <th className="text-xl" >level</th>
             <th className="text-xl" >Job_id</th>
             <th className="text-xl" >Time</th>
-
-
           </tr>
         </thead>
         <tbody>
-          {rows.map((row, index) => {
-            // const correctedJson = row.log.replace(/'/g, '"');
-            // let logObj = JSON.parse(correctedJson);
-
-            return (
-              <tr key={index}>
-                <th>{index + 1}</th>
-                <td>{row.Job}</td>
-                <td>{row.message}</td>
-                <td>{row.level}</td>
-                <td>{row.Job_id}</td>
-                <td>{new Date(row.time * 1000).toLocaleString()}</td>
-
-              </tr>
-            );
-          })}
-
+          {rows.map((row, index) => (
+            <tr key={index}>
+              <th>{index + 1}</th>
+              <td>{row.Job}</td>
+              <td>{row.message}</td>
+              <td>{row.level}</td>
+              <td>{row.Job_id}</td>
+              <td>{new Date(row.time * 1000).toLocaleString()}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
-  </>
   )
 }
--- front-end-ui-v2/src/app/user/risk/page.js
+++ front-end-ui-v2/src/app/user/risk/page.js
@@ -1,20 +1,88 @@
 "use client"
-import React from 'react'
-import NavBar from '@/components/NavBar';
+
+import React, { useEffect, useState, useLayoutEffect } from 'react';
 import SideBar from '@/components/SideBar';
+import NavBar from '@/components/NavBar';
 
+import Chart from 'chart.js/auto';
 
+export default function Page() {
+  function CreateGraph(labelsPlusData, elmId, graphType) {
+    const ctx = document.getElementById(elmId);
+    if (window.myChartInstance) {
+      window.myChartInstance.destroy();
+    }
 
+    window.myChartInstance = new Chart(ctx, {
+      type: graphType,
+      data: {
+        labels: Object.keys(labelsPlusData),
+        datasets: [{
+          data: Object.values(labelsPlusData)
+        }]
+      }
+    });
+  }
 
+  const [rows, setRows] = useState([]);
+  const [data, setData] = useState([]);
 
-export default function page() {
+  useEffect(() => {
+    async function fetchJobDetails() {
+      try {
+        const response = await fetch('http://127.0.0.1:223/Job_details');
+        const data = await response.json();
+        console.log("Fetched data");
+
+        setRows((currentRows) => {
+          if (JSON.stringify(currentRows) !== JSON.stringify(data)) {
+            setData(data);
+            return data;
+          }
+          return currentRows;
+        });
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchJobDetails();
+    const intervalId = setInterval(fetchJobDetails, 5000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useLayoutEffect(() => {
+    console.log(document.getElementById("canva-doughnut")); // Check if canvas element exists
+    console.log(CountValuesInArray(data)); // Verify data is being passed correctly
+
+    CreateGraph(CountValuesInArray(data), "canva-doughnut", "doughnut");
+    CreateGraph(CountValuesInArray(data), "canva-bar", "bar");
+  }, [data]);
+
+  function CountValuesInArray(rows) {
+    let dict = {};
+    rows.forEach((row) => {
+      let what = row.level;
+      dict[what] = (dict[what] || 0) + 1;
+    });
+
+    return dict;
+  }
+
   return (
     <div>
       <NavBar />
-      <div className='flex h-screen'>
-      </div>
 
+      <div className='flex h-screen justify-evenly '>
+        <div>
+          <canvas id="canva-doughnut"  ></canvas>
+        </div>
+        <div>
+          <canvas id="canva-bar"></canvas>
+        </div>
+
+      </div>
       <SideBar />
     </div>
-  )
+  );
 }
