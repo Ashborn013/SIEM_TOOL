8248c2b97bc1ffb02ac1a2a1765a313384d22e49
Ashborn013 <csarjun11@gmail.com>
2024-08-29 20:43:54 +0530

    deleted the old Ui Project

--- front-end-ui/pages/api/hello.js
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { serialize } from 'cookie'
-
-export default function hello(req, res) {
-  res.status(200).json({ name: "John Doe" });
-}
